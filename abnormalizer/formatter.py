b24fc09dea1e1cc3c8fc4a7f4468aa5d727d9610
nic james <nic@nicjame.es>
2019-03-15 18:15:44 -0700

    adding README, trailing whitespace removal works more

--- abnormalizer/formatter.py
+++ abnormalizer/formatter.py
@@ -1,5 +1,5 @@
 from pygments.formatter import Formatter
-from pygments.token import *
+from pygments.token import Token as PygmentsToken
 from collections import namedtuple
 import logging
 import os 
@@ -24,12 +24,23 @@ class Token(object):
         self.value = value
 
     def remove_trailing_whitespace(self) -> None:
-        self.value = "\n".join([line.rstrip() for line in self.value.split("\n")])
+        if "\n" in self.value and len(self.value) > 1:
+            logger.info(f"removing trailing whitespace from {self.ttype}")
+            logger.info(f"before: `{self}`")
+            self.value = "\n".join([line.rstrip() for line in self.value.split("\n")])
+            logger.info(f"after: `{self}`")
+    
+    def __str__(self):
+        value = self.value
+        value = value.replace('\t', '⇥⇥⇥⇥')
+        value = value.replace('\n', '␤\n')
+        value = value.replace(' ', '⍽')
+        return f"{self.ttype}: `{value}`"
 
 LOGLEVEL = logging.DEBUG
 log_file_path = os.path.expanduser("~/formatter_logs.txt")
 logger = logging.getLogger(__name__)
-filehandler = logging.FileHandler(filename=log_file_path)
+filehandler = logging.FileHandler(filename=log_file_path, mode='w')
 filehandler.setLevel(LOGLEVEL)
 logger.addHandler(filehandler)
 logger.setLevel(LOGLEVEL)
@@ -51,20 +62,28 @@ class NormeFormatter(Formatter):
         tokens = [Token(ttype=ttype, value=value) for ttype, value in tokensource]
         clean_tokens = []
         for idx, token in enumerate(tokens):
+            logger.info(token)
             token.remove_trailing_whitespace()
-            logger.info(f"{token.ttype}:\n{token.value}\n")
+            if token.ttype == PygmentsToken.Comment.Preproc:
+                pass
 
             if (token.value == '\n' and idx > 0 and tokens[idx-1].value == '\n'):
                 logger.info(f"trimming newline at token idx {idx}")
                 continue
             else:
                 clean_tokens.append(token)
+            
+            if token.ttype == PygmentsToken.Comment.Preproc:
+                if "#if" in token.value:
+                    self.preprocessor_define_depth += 1
+                elif "#endif" in token.value:
+                    self.preprocessor_define_depth -= 1
         return clean_tokens
 
     def format(self, tokensource, outfile):
         tokens = self.preformat(tokensource)
         for token in tokens:
-            if (token.ttype == Comment.Special):
+            if (token.ttype == PygmentsToken.Comment.Special):
                 outfile.write(IM_SO_SORRY)
                 continue;
             outfile.write(token.value)
